de15c4bb39cc669ecf58a265e8ef2ae4863a9044
rcmehedi15 <hmdmehedi2016@gmail.com>
2023-05-12 12:50:25 +0600

    ongodb connect

--- src/pages/Home/Services/ServiceCard.jsx
+++ src/pages/Home/Services/ServiceCard.jsx
@@ -1,7 +1,8 @@
+import { Link } from "react-router-dom";
 
 
 const ServiceCard = ({ service }) => {
-    const { title, img, price } = service;
+    const { _id,title, img, price } = service;
     return (
         <div className="card w-96 bg-base-100 shadow-xl">
             <figure className="px-10 pt-10">
@@ -11,8 +12,10 @@ const ServiceCard = ({ service }) => {
                 <h2 className="card-title">{title}</h2>
                 <p className="text-xl text-orange-500">Price: ${price}</p>
                 <div className="card-actions">
-                    
-                    <button className="btn border-none bg-[#FF3811] text-white">Buy Now</button>
+
+                    <Link to={`/checkout/${_id}`}>
+                        <button className="btn border-none bg-[#FF3811] text-white">Book Now</button>
+                    </Link>
                 </div>
             </div>
         </div>
